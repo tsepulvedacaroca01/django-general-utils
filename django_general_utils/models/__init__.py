cf7c5d6b74acd3752a146581bb53cf1f972137d6
Tomás Sepúlveda <t.sepulvedacaroca@gmail.com>
2024-12-27 14:23:22 -0300

    Se ha generado la descarga del historial de movimientos

--- django_general_utils/models/__init__.py
+++ django_general_utils/models/__init__.py
@@ -1,2 +1,4 @@
 from .base import BaseModel, SignalRegister
+from .base_without_safe_delete import BaseWithoutSafeDeleteModel
+from .base_without_safe_delete import BaseWithoutSafeDeleteModel as BaseV2
 from .uuid import UUIDModel
--- django_general_utils/models/constraints/check_flow_status.py
+++ django_general_utils/models/constraints/check_flow_status.py
@@ -14,6 +14,7 @@ class CheckFlowStatusConstraint(BaseConstraint):
             initial_statuses = None,
             validate_on_create=True,
             validate_on_update=True,
+            skip_validate=None,
             violation_error_message=None,
             violation_error_initial_statuses_message=None
     ):
@@ -22,6 +23,8 @@ class CheckFlowStatusConstraint(BaseConstraint):
         self.initial_statuses = initial_statuses
         self.validate_on_create = validate_on_create
         self.validate_on_update = validate_on_update
+        self.skip_validate = skip_validate
+        self.violation_error_message = violation_error_message
         self.violation_error_initial_statuses_message = violation_error_initial_statuses_message
 
         super().__init__(name=name, violation_error_message=violation_error_message)
@@ -54,7 +57,11 @@ class CheckFlowStatusConstraint(BaseConstraint):
             return None
 
         current_value = getattr(instance, self.field)
+        last_value = instance.tracker.previous(self.field)
         choices = dict(model._meta.get_field(self.field).choices)
+
+        if self.skip_validate is not None and self.skip_validate(instance, last_value, current_value):
+            return None
 
         # If the instance is being created and the initial statuses are defined, validate that the current value is in the initial statuses
         if (
@@ -79,10 +86,12 @@ class CheckFlowStatusConstraint(BaseConstraint):
         if not self.validate_on_update and not instance._state.adding:
             return None
 
-        last_value = instance.tracker.previous(self.field)
-
         # If the instance is being created and the last value is None, return None
         if last_value is None and instance._state.adding:
+            return None
+
+            # If the las value is None and the instance is not being created, return None
+        if last_value is None and not instance._state.adding:
             return None
 
         # If the last value is equal to the current value, return None
@@ -91,14 +100,15 @@ class CheckFlowStatusConstraint(BaseConstraint):
 
         # If the current value is not in the flow of the last value, raise a validation error
         if current_value not in self.flow.get(last_value, []):
+            violation_error_initial_statuses_message = self.violation_error_initial_statuses_message or {
+                'status': 'No puedes avanzar este estado a "{current_value}" debido a que su valor actual "{last_value}" no lo permite.',
+            }
             message = {
                 _key: _value.format(
                     current_value=current_value,
                     last_value=last_value,
-                    current_value_display=choices.get(current_value),
-                    last_value_display=choices.get(last_value),
                 )
-                for _key, _value in self.violation_error_message.items()
+                for _key, _value in violation_error_initial_statuses_message.items()
             }
 
             raise ValidationError(message)
@@ -108,12 +118,14 @@ class CheckFlowStatusConstraint(BaseConstraint):
     def __eq__(self, other):
         if isinstance(other, CheckFlowStatusConstraint):
             return (
-                    self.flow == other.flow
-                    and self.field == other.field
+                    self.field == other.field
+                    and self.flow == other.flow
                     and self.initial_statuses == other.initial_statuses
                     and self.validate_on_create == other.validate_on_create
                     and self.validate_on_update == other.validate_on_update
+                    and self.skip_validate == other.skip_validate
                     and self.violation_error_message == other.violation_error_message
+                    and self.violation_error_initial_statuses_message == other.violation_error_initial_statuses_message
             )
         return super().__eq__(other)
 
@@ -121,11 +133,14 @@ class CheckFlowStatusConstraint(BaseConstraint):
         path, args, kwargs = super().deconstruct()
 
         kwargs.update({
-            'flow': self.flow,
             'field': self.field,
+            'flow': self.flow,
             'initial_statuses': self.initial_statuses,
             'validate_on_create': self.validate_on_create,
             'validate_on_update': self.validate_on_update,
+            'skip_validate': self.skip_validate,
+            'violation_error_message': self.violation_error_message,
+            'violation_error_initial_statuses_message': self.violation_error_initial_statuses_message,
         })
 
         return path, args, kwargs
